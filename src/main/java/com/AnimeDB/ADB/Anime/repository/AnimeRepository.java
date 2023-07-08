8972139ec77aa0add190d30e391108ccf8680fc8
danielkiko <danielkiko@yandex.ru>
2023-07-08 14:26:49 +0300

    починил несчастный список аниме -_-

--- src/main/java/com/AnimeDB/ADB/Anime/repository/AnimeRepository.java
+++ src/main/java/com/AnimeDB/ADB/Anime/repository/AnimeRepository.java
@@ -1,4 +1,11 @@
 package com.AnimeDB.ADB.Anime.repository;
 
-public interface AnimeRepository {
+import com.AnimeDB.ADB.Anime.entity.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AnimeRepository extends JpaRepository<Anime, UUID> {
 }
